31aa35431d83285aa8915f321e0ba0626a865bc8
Edwin Rivera <54447566+EdRiveraO@users.noreply.github.com>
2019-11-19 22:36:09 -0500

    Disconnect/style

--- index.js
+++ index.js
@@ -23,12 +23,14 @@ io.on('connection', function(socket){
 
   socket.name=username;
  
-  socket.broadcast.emit('message',socket.name+' se ha conectado','System');
+  socket.broadcast.emit('message',socket.name+' se ha conectado','');
   socket.on('message',function(msj){
     io.emit('message',msj,socket.name);
   });
 
+
   socket.on('disconnect',function(e){
+    socket.broadcast.emit('message',socket.name+' se ha desconectado','');
     console.log("desconectado: %s",socket.name);
   });
 });
